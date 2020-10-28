138b54761c5a0dfc1ca452761958e22068fe4ef8
Siruvuru <aravind.siruvuru@acsicorp.com>
2020-10-28 14:42:46 +0530

    working copy auth toggle null and complete login flow

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:pollocksschool/utils/config/strings.dart';
+import 'package:pollocksschool/utils/config/styling.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        image:  DecorationImage(
-          image: AssetImage(Strings.getSplashImagePath),
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: SpinKitChasingDots(
+          color: AppTheme.primaryColor,
+          size: 50.0,
         ),
-
       ),
     );
   }
--- lib/utils/config/strings.dart
+++ lib/utils/config/strings.dart
@@ -2,7 +2,6 @@ class Strings {
   Strings._();
 
   static const String getLogoImagePath = "assets/images/pollocks_logo.png";
-  static const String getSplashImagePath = "assets/images/splash_img.png";
   static const String getPasswordErrorText =
       '''     min 6 characters and contains atleast one
      special character like !@#\$%^&*(),.?":{}|<>    ''';
