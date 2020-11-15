2338055022a7a940f123388132268a613de211e9
Siruvuru <aravind.siruvuru@acsicorp.com>
2020-11-15 22:47:21 +0530

    save test

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pollocksschool/utils/config/size_config.dart';
+import 'package:pollocksschool/utils/config/strings.dart';
 
 class DashboardScreen extends StatelessWidget {
   @override
@@ -9,10 +10,16 @@ class DashboardScreen extends StatelessWidget {
         padding: EdgeInsets.all(SizeConfig.heightMultiplier * 3),
         child: GridView.count(
           crossAxisCount: 3,
+          mainAxisSpacing: SizeConfig.heightMultiplier * 3,
+          crossAxisSpacing: SizeConfig.heightMultiplier * 3,
           children: List.generate(7 ,(index){
-            return Container(
-              child: Card(
-                color: Colors.blue,
+            return InkWell(
+              onTap: () => print("tappeed"),
+              child: Container(
+                margin: EdgeInsets.all(SizeConfig.heightMultiplier),
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage(Strings.getLogoImagePath))
+                ),
               ),
             );
           }),
